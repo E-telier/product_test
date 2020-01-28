d09b9fb2741ae7074b5e7ec8e908beb95b33dbd1
E-telier <contact@e-telier.be>
2020-01-28 14:21:24 +0100

    PRODUCT DATA FORMAT DOUBLE CHECKED IN API

--- src/Controller/RestController.php
+++ src/Controller/RestController.php
@@ -20,8 +20,21 @@ class RestController extends AbstractController
         $product_id = $request->request->get('product_id', '');
         $product_manager = $request->request->get('product_manager', '');
         $product_date = $request->request->get('product_date', '');
-        if (!empty($product_name) && !empty($product_id) && !empty($product_date)) {
+        $valid = false;
 
+        // CHECK REQUIRED //
+        if (!empty($product_name) && !empty($product_id) && !empty($product_date)) {
+            // CHECK NUMERIC //
+            if (is_numeric($product_id)) {
+                // CHECK DATE FORMAT //
+                $date_format = 'Y-m-d';
+                $d = \DateTime::createFromFormat($date_format, $product_date);
+                if ($d && $d->format($date_format) == $product_date) {
+                    $valid = true;
+                }
+            }
+        }
+        if ($valid) {
             // POST THE PRODUCT //
             $result = JSON_decode('{"result": 1}');
             return new JsonResponse($result, Response::HTTP_OK);
@@ -32,7 +45,7 @@ class RestController extends AbstractController
             $result = JSON_decode('{"result": 0}');
             return new JsonResponse($result, Response::HTTP_BAD_REQUEST);
 
-        }       
+        }
 
     }
 
